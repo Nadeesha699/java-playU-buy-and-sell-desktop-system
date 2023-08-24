86b5c8f3726562c5f84b2e77fba40a6c95ea5eda
Nadeesha_xxn <114317388+Nadeesha699@users.noreply.github.com>
2023-08-24 13:49:26 +0530

    gg

---
+++ playu_two/PlayU_Two.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playu_two;
+
+import playu_two.newpackage.Login;
+
+/**
+ *
+ * @author HP
+ */
+public class PlayU_Two {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login obj = new Login();
+        obj.show();
+    }
+    
+}
